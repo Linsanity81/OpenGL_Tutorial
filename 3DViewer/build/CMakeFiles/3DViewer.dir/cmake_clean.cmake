62fb5026b4d532530b8679e6b8f36518690252aa
Linsanity81 <37648478+Linsanity81@users.noreply.github.com>
2023-08-17 13:10:42 +0800

    update

---
+++ 3DViewer/build/CMakeFiles/3DViewer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "3DViewer"
+  "3DViewer.pdb"
+  "CMakeFiles/3DViewer.dir/src/main.cpp.o"
+  "CMakeFiles/3DViewer.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/3DViewer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ 3DViewer/build/CMakeFiles/Assignment_1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "Assignment_1"
+  "Assignment_1.pdb"
+  "CMakeFiles/Assignment_1.dir/src/main.cpp.o"
+  "CMakeFiles/Assignment_1.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Assignment_1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
